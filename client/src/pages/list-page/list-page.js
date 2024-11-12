4bd171502ff67550f16577e14e0ed9c4eb58e6e4
Sercan Sever <sercan.sever16@gmail.com>
2024-11-12 16:07:09 +0300

    add list page with filter and card components; implement dummy data for listings

--- client/src/pages/list-page/list-page.js
+++ client/src/pages/list-page/list-page.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./list-page.scss";
+import { listData } from "../../lib/dummy-data.js";
+import Filter from "../../components/filter/filter.js";
+import Card from "../../components/card/card.js";
 
 function ListPage() {
+  const data = listData;
   return (
     <div className="listPage">
-      ListPage
-      <p>Test</p>
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} data={item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">Map</div>
     </div>
   );
 }
