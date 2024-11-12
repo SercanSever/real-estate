5f0279f48f0d8317f8ede46f0681273cde39ad11
Sercan Sever <sercan.sever16@gmail.com>
2024-11-12 17:41:51 +0300

    refactor list page and card components; enhance styling and layout for better user experience

--- client/src/components/filter/filter.js
+++ client/src/components/filter/filter.js
@@ -6,54 +6,64 @@ const Filter = () => {
     <div className="filter">
       <h1>
         Search result for <b>London</b>
-        <div className="top">
-          <div className="item">
-            <label htmlFor="type">Location</label>
-            <select name="type" id="type">
-              <option value="any">Any</option>
-              <option value="buy">Buy</option>
-              <option value="rent">Rent</option>
-            </select>
-          </div>
-        </div>
-        <div className="bottom">
-          <div className="item">
-            <label htmlFor="property">Property</label>
-            <select name="property" id="property">
-              <option value="any">Any</option>
-              <option value="apartment">Apertment</option>
-              <option value="house">House</option>
-              <option value="condo">Condo</option>
-              <option value="land">Lnad</option>
-            </select>
-          </div>
-          <div className="item">
-            <label htmlFor="minPrice">Min Price</label>
-            <input
-              type="number"
-              id="minPrice"
-              name="minPrice"
-              placeholder="any"
-            />
-          </div>
-          <div className="item">
-            <label htmlFor="maxPrice">Max Price</label>
-            <input
-              type="number"
-              id="maxPrice"
-              name="maxPrice"
-              placeholder="any"
-            />
-          </div>
-          <div className="item">
-            <label htmlFor="bedroom">Bedroom</label>
-            <input type="text" id="bedroom" name="bedroom" placeholder="any" />
-          </div>
-          <button>
-            <img src="/search.png" alt="" />
-          </button>
-        </div>
       </h1>
+
+      <div className="top">
+        <div className="item">
+          <label htmlFor="city">Location</label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            placeholder="City Location"
+          />
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="item">
+          <label htmlFor="type">Type</label>
+          <select name="type" id="type">
+            <option value="any">Any</option>
+            <option value="buy">Buy</option>
+            <option value="rent">Rent</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="property">Property</label>
+          <select name="property" id="property">
+            <option value="any">Any</option>
+            <option value="apartment">Apertment</option>
+            <option value="house">House</option>
+            <option value="condo">Condo</option>
+            <option value="land">Lnad</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="minPrice">Min Price</label>
+          <input
+            type="number"
+            id="minPrice"
+            name="minPrice"
+            placeholder="any"
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="maxPrice">Max Price</label>
+          <input
+            type="number"
+            id="maxPrice"
+            name="maxPrice"
+            placeholder="any"
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="bedroom">Bedroom</label>
+          <input type="text" id="bedroom" name="bedroom" placeholder="any" />
+        </div>
+        <button>
+          <img src="/search.png" alt="" />
+        </button>
+      </div>
     </div>
   );
 };
